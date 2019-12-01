5d21ce64c41abcdaddb10ff6cb180b0fbf88727d
Krisha Moeller <thefluffyshrimp@Krishas-MacBook-Air.local>
2019-12-01 02:54:50 -0800

    my comment

--- theme/moove/classes/util/icon_system.php
+++ theme/moove/classes/util/icon_system.php
@@ -204,7 +204,6 @@ class icon_system extends icon_system_font {
             'core:i/competencies' => 'slicon-check',
             'core:i/completion_self' => 'fa-user-o',
             'core:i/dashboard' => 'slicon-speedometer',
-            'core:i/lock' => 'fa-lock',
             'core:i/categoryevent' => 'fa-cubes',
             'core:i/course' => 'slicon-graduation',
             'core:i/courseevent' => 'fa-university',
@@ -399,7 +398,7 @@ class icon_system extends icon_system_font {
      */
     public function get_icon_name_map() {
         if ($this->map === []) {
-            $cache = \cache::make('core', 'fontawesomeiconmapping');
+            $cache = \cache::make('theme_moove', 'fontawesomemooveiconmapping');
 
             $this->map = $cache->get('mapping');
 
--- theme/moove/lang/en/theme_moove.php
+++ theme/moove/lang/en/theme_moove.php
@@ -37,7 +37,7 @@ $string['search_site'] = 'Search on site';
 $string['search_forums'] = 'Search on course forums';
 $string['platform_access'] = 'Access to the platform';
 $string['readmore'] = 'Read More';
-$string['stayintouch'] = '';
+$string['stayintouch'] = 'Stay in touch';
 $string['madewitmoodle'] = 'Proudly made with';
 $string['madeby'] = 'Made with';
 $string['by'] = 'by';
@@ -60,7 +60,7 @@ $string['favicondesc'] = 'Upload your own favicon.  It should be an .ico file.';
 $string['preset'] = 'Theme preset';
 $string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
 $string['presetfiles'] = 'Additional theme preset files';
-$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href="https://docs.moodle.org/dev/Boost_Presets">Boost presets</a> for information on creating and sharing your own preset files.';
 $string['loginbgimg'] = 'Login page background';
 $string['loginbgimg_desc'] = 'Upload your custom background image for the login page.';
 $string['brandcolor'] = 'Brand colour';
@@ -89,6 +89,8 @@ $string['googleanalyticsdesc'] = 'Please enter your Google Analytics code to ena
 
 // Frontpage settings tab.
 $string['frontpagesettings'] = 'Frontpage';
+$string['disablefrontpageloginbox'] = 'Disable frontpage login box';
+$string['disablefrontpageloginboxdesc'] = 'If you check it, the frontpage login box will be hidden and a link to the login page will be printed in the header bar.';
 $string['headerimg'] = 'Header image';
 $string['headerimgdesc'] = 'Upload your custom header-image here if you want to add it to the header.mage works best if (Image size should be 1500px x 700px)';
 $string['bannerheading'] = 'Banner Heading';
