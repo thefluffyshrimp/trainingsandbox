2c3c9c82c41a713fc9b6dd3a70bc9f5dfd56c02a
avinash <avinash.yashco@gmail.com>
2021-09-24 15:28:54 +0530

    update 3.8.9

--- version.php
+++ version.php
@@ -7,6 +7,7 @@
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
+
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
